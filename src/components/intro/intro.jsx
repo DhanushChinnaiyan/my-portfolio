958aba59a1bc3ebabb8d1ee65fab38ac2ee4b862
DhanushChinnaiyan <dhanushmd4021@gmail.com>
2023-08-14 15:52:34 +0530

    updated resume uploaded

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -56,7 +56,7 @@ const Intro = () => {
 
                     <div className='buttondiv'>
 
-                        <a href="https://drive.google.com/file/d/1H7Tlsc42bExOywbEQhEerQU7FrlpDR-O/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
+                        <a href="https://drive.google.com/file/d/1HD4MUWmzSHLK9LPw7B3enikX7HMV4_9X/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
                             <button className="button intro-button">Resume</button>
                         </a>
                     </div>
